2c1537aa22d21424619f0ff4e9936517daa2558a
ramotheonly <ramotheonly@gmail.com>
2020-12-27 12:21:33 -0800

    separated discord page and added mqtt page

---
+++ mqtt/config.js
@@ -0,0 +1,19 @@
+// host = '192.168.1.27';	// hostname or IP address
+host = '127.0.0.1';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+port = 9001;        //default wwebsockets port
+topic = '#';		// topic to subscribe to
+useTLS = false;
+username = null;
+password = null;
+// username = "user";
+// password = "passwd";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
